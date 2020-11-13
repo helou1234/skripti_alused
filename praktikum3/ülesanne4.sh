8464d8e80020cc43cd0bc385b9a29ea1fe57458a
Helena Õunapuu <helena.ounapuu@khk.ee>
2020-11-13 10:33:10 +0200

    bussi täituvuse arvutamine kasutaja andmetega

---
+++ praktikum3/ülesanne4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+###***************Skript, mis uurib reisijate arvu ja kohtade arvu ning arvutab välja, mitut bussi on tarvis******
+echo
+echo
+echo -n "Palun sisestage reisijate arv: "
+read reisijad
+echo -n "Palun sisestage kohtade arv bussis: "
+read kohad
+bussid=$(("$reisijad/$kohad"))
+eimahu=$(($reisijad-$bussid*$kohad))
+echo "Ekskursioonile saab tulla $bussid bussitäit inimesi. Retkelt jääb maha $eimahu inimest"
