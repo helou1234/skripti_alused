a3944e24f77f0af783b1e639a4be13f30053ad48
Helena Õunapuu <helena.ounapuu@khk.ee>
2020-11-13 10:00:24 +0200

    lause moodustamine

---
+++ praktikum3/ülesanne2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#*******************Skriptis liidame numbrid ja sõned lauseks**********
+aasta=2016
+president="Kersti Kaljulaid"
+lause_keskosa=". aastal valiti Eesti presidendiks "
+lause="$aasta$lause_keskosa$president"
+echo  $lause
