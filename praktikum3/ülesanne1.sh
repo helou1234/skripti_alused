0c076401e4d34a3e35afbf0de7718fab1faa10ab
Helena Õunapuu <helena.ounapuu@khk.ee>
2020-11-13 09:45:07 +0200

    kasutajalt andmete küsimine ja kuvamine

---
+++ praktikum3/ülesanne1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#************Skriptis küsime kasutajalt tema ees- ja perenime ning kuvame need************
+echo
+echo
+echo "Palun sisestage enda ees- ja perenimi: "
+read nimed
+echo " Tere tulemast, $nimed!"
+echo
+echo
