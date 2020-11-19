1f6530cd3345d282dd56756a2614bbd98794c19e
Helena Õunapuu <helena.ounapuu@khk.ee>
2020-11-19 12:29:04 +0200

    skript ütleb, millised 20-st täisarvust on algarvud

--- praktikum6/ülesanne4.sh
+++ praktikum6/ülesanne4.sh
@@ -2,28 +2,37 @@
 #**********Skript, mis tuvastab, kas kasutaja sisestatud arv on algarv või mitte
 echo
 echo
-echo -n "Palun sisestage üks arv: "
-read arv
-if [ $arv = 0 ] || [ $arv = 1 ] && [ $arv -le 20 ]; then
-	echo "$arv ei ole algarv"
-elif [ $arv -gt 20 ]; then
-	echo "Kahjuks nii suurt arvu me ei kontrolli!"
-else
-	jagaja=2
-#	jaak=$(($arv % $jagaja)) #####kasutasin testimiseks ja koodi mõistmiseks õpetaja kirjutatut
-	while (( $arv%$jagaja!=0 )) && [ $arv -le 20 ]; do ###liidan tehte argumenti
+#echo -n "Palun sisestage üks arv: "
+#read arv
+#if [ $arv = 0 ] || [ $arv = 1 ] && [ $arv -le 20 ]; then
+#	echo "$arv ei ole algarv"
+#elif [ $arv -gt 20 ]; then
+#	echo "Kahjuks nii suurt arvu me ei kontrolli!" #ainuke õige paigutus sellele käsule, kuid võtame hetkel maha, kuna see polnud ülesandes vajalik
+#else
+#	jagaja=2
+#	while (( $arv%$jagaja!=0 )) && [ $arv -le 20 ]; do ###liidan tehte argumenti
 ###Kui kasutan () sulge, võin sinna panna tehte, kui kasutan [] sulge, pean kirjutama tehte sõne kujul
-		jagaja=$(($jagaja + 1))
-#		jaak=$(($arv % $jagaja)) ####kasutasin testimiseks õpetaja lahendust ja nüüd kommenteerisin need välja
-	done
-	if [ $arv -eq $jagaja ]; then
-		echo "$arv on algarv"
-#	elif [ $arv -gt 20]; then
-#		echo "Kahjuks nii suurt arvu me ei kontrolli!"
-	else
-		echo "$arv ei ole algarv"
-	fi
-#elif [ $arv -gt 20]; then
-#               echo "Kahjuks nii suurt arvu me ei kontrolli!"
+#		jagaja=$(($jagaja + 1))
+#	done
+#	if [ $arv -eq $jagaja ]; then
+#		echo "$arv on algarv"
+#	else
+#		echo "$arv ei ole algarv"
+#	fi
+#fi
 
-fi
+for (( arv=1; arv<=20; arv++)); do
+	if [ $arv = 1 ]; then
+	       echo "$arv ei ole algarv"
+	else
+		jagaja=2
+      		while (( $arv%$jagaja!=0 )); do
+	               jagaja=$(($jagaja + 1))
+		done
+		if [ $arv -eq $jagaja ]; then
+			echo "$arv on algarv"
+      		else
+	        	echo "$arv ei ole algarv"
+		fi
+	fi
+done
