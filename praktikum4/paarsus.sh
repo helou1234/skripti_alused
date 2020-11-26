2a35f96ed168cb5d0c65b3f928527d04d40acdbc
Helena Õunapuu <helena.ounapuu@khk.ee>
2020-11-26 14:15:21 +0200

    küsib parameetrina arvu ja väljastab puudumisel veateate

--- praktikum4/paarsus.sh
+++ praktikum4/paarsus.sh
@@ -3,11 +3,18 @@
 echo
 #echo "Sisesta arv, mille paarsust soovid kontrollida: "
 #read arv - need kaks rida kommenteerime välja, kuna skript võtab argumendi command line'ist
-jagatis=$(($1%2))
-if [ $jagatis -eq 0 ]
-then
-        echo "Tegu on paarisarvuga!"
+#jagatis=$(($1%2))
+#if [ $jagatis -eq 0 ]; then
+#        echo "Tegu on paarisarvuga!"
+#elif [ $jagatis -eq 1 ]; then
+#        echo "Tegu on paaritu arvuga!"
+if [ $# -eq 0 ]; then
+	echo "Palun sisestage parameeter!"
 else
+	jagatis=$(($1%2))
+	if [ $jagatis -eq 0 ]; then
+        echo "Tegu on paarisarvuga!"
+	elif [ $jagatis -eq 1 ]; then
         echo "Tegu on paaritu arvuga!"
+	fi
 fi
-
